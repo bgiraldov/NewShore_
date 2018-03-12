d575c735a97132ed54823a49081b79efae7028ad
Bryam Giraldo <bgiraldo@consensussa.com>
2018-03-12 17:04:05 -0500

    Agrega Test Unit

---
+++ WCFTests/ServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WCF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF.Tests
+{
+    [TestClass()]
+    public class ServiceTests
+    {
+        [TestMethod()]
+        public void EsClienteTest()
+        {
+            string expected = "TZEAVHLMOFOUEBYOPDRBAADILNGVDUEKWATZLOTTONTUT";
+            string actual = "VOLOTEA";
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
